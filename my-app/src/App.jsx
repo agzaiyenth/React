0b44717c85366a3c40a46dbc7a4c51e14cb6ba98
Agzaiyenth Ganaraj <agzaiyenth2@gmail.com>
2024-06-21 21:55:58 +0530

    edits

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,18 @@
-
+import Header from "./Header"
+import Footer from "./Footer";
 
 function App() {
  
+  return(
+
+    <>
+    <Header></Header>
+    <Footer></Footer>
+    </>
+    
+
+  );
+
 }
 
 export default App
