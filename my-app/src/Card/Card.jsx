e530905d1408a10ef4487a6c7b2f0f1a94df4967
Agzaiyenth Ganaraj <agzaiyenth2@gmail.com>
2024-06-23 21:56:40 +0530

    Update Card.jsx

--- my-app/src/Card/Card.jsx
+++ my-app/src/Card/Card.jsx
@@ -2,7 +2,7 @@ function Card(){
     return(
         <div className="card">
 
-            <img src=""></img>
+            <img src="../assets/react.svg" />
             <h2>My card</h2>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis, molestias!</p>
             
