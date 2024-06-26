63ba9625129611362380eac3e79fb80d829b34f0
Agzaiyenth Ganaraj <agzaiyenth2@gmail.com>
2024-06-26 14:19:02 +0530

    juagsd

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,4 +1,6 @@
-import Header from "./Header"
+
+import Card from "./Card/Card";
+import Header from "./Header";
 import Footer from "./Footer";
 
 function App() {
@@ -6,8 +8,11 @@ function App() {
   return(
 
     <>
-    <Header></Header>
     <Footer></Footer>
+    
+    <Card name="Agziyenth" profilePic="../src/assets/20230746.jpg"></Card>
+    <Card name="Asra Ameer" profilePic="../src/assets/golden.retriever.clubs_1718805598217.jpeg"></Card>
+    <Header></Header>
     </>
     
 
--- my-app/src/Card/Card.jsx
+++ my-app/src/Card/Card.jsx
@@ -1,12 +1,18 @@
-function Card(){
-    return(
-        <div className="card">
 
-            <img src="../assets/react.svg" />
-            <h2>My card</h2>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis, molestias!</p>
+
+import Styles from "../Card/card.module.css"
+
+function Card(props){
+    return(
+        <div className={Styles.card}>
+
+            <img src={props.profilePic} />
+            <h2>{props.name}</h2>
+            <hr/>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi, ipsam! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis, molestias!</p>
             
         </div>
+        
     );
 }
 export default Card
