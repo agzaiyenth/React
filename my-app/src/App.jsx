a3ca4dbe5d04bfb53d541e83d60767906e77526f
Agzaiyenth Ganaraj <agzaiyenth2@gmail.com>
2024-07-07 15:30:34 +0530

    Update App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <Footer></Footer>
     
     <Card name="Agzaiyenth" profilePic="../src/assets/20230746.jpg"></Card>
-    <Card name="Ameer Ameer" profilePic="../src/assets/golden.retriever.clubs_1718805598217.jpeg"></Card>
+    <Card name="Ameer adnan Ameer" profilePic="../src/assets/golden.retriever.clubs_1718805598217.jpeg"></Card>
     <Header></Header>
     </>
     
